441543aad76d5cc2ac65a72cc1661416a90b2ff0
Maxime Robert <maxime.robert1992@gmail.com>
2018-06-28 15:25:37 +0100

    chore: disable hot reloading and use page reload instead

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { Engine } from 'babylonjs';
+declare const module;
+
+// avoid hot reload with Parcel
+// https://github.com/parcel-bundler/parcel/issues/289#issuecomment-393106708
+if (module.hot) {
+  module.hot.dispose(() => {
+    window.location.reload();
+  });
+}
 
 console.log(Engine);
